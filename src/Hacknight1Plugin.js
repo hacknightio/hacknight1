9e8ef16380bbf4012ab9f61fa2861c873b14eaf6
Jan Taylor <janttaylor@gmail.com>
2018-11-15 21:25:15 -0700

    Add api

--- src/Hacknight1Plugin.js
+++ src/Hacknight1Plugin.js
@@ -28,9 +28,15 @@ export default class Hacknight1Plugin extends FlexPlugin {
       <CustomTaskInfoPanelItem key="steps-for-task" />
     );
     flex.CRMContainer.defaultProps.uriCallback = task => {
-      return task
-        ? "https://www.bing.com/search?q=" + task.attributes.name
-        : "https://bing.com/";
+      const data = task
+        ? `http://apilayer.net/api/validate?access_key=e18ed9c6e2ffc48d1dc262fb1cb6ebe2&number=${
+            task.attributes.name
+          }`
+        : {};
+
+      return `https://www.google.com/maps/@?api=1&map_action=map&query=${encodeURI(
+        data.location
+      )}`;
     };
   }
 }
