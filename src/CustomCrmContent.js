18b8ece79d6d675926ffdc9731a2131aef14cfae
Jan Taylor <janttaylor@gmail.com>
2018-11-18 23:56:52 -0700

    Getting Map data from numVerify with Tyler Liiveshare

--- src/CustomCrmContent.js
+++ src/CustomCrmContent.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import NumberInformationComponent from "./NumberInformationComponent";
 import LocationInformationComponent from "./LocationInformationComponent";
+export const accessToken =
+  "pk.eyJ1IjoiamFudGF5bG9yIiwiYSI6ImNqb2ppdWRjZDA1bTEzd21uNW1kYzJwNGIifQ.KfkXqIdmLVrxByMnpb57kA";
 
+/* Now we know we would never post API codes, but we can reset these and it's on trial accounts :) */
 const numVeryifyUrl =
   "http://apilayer.net/api/validate?access_key=e18ed9c6e2ffc48d1dc262fb1cb6ebe2&number=";
 
 export default class CustomCrmContent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      numVerify: {},
+      location: {}
+    };
   }
+
+  mapBoxGeoUrl = numVerify =>
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(
+      numVerify.location
+    )}.json?access_token=${accessToken}&country=${
+      numVerify.country_code
+    }&autocomplete=true&limit=1`;
 
   async componentDidUpdate(prevProps) {
     if (this.props.task !== prevProps.task) {
@@ -19,15 +32,27 @@ export default class CustomCrmContent extends Component {
         this.props.task.attributes &&
         this.props.task.attributes.name;
       if (phoneNumber) {
-        const blah = await fetch(`${numVeryifyUrl}${phoneNumber}`);
-        const numVerify = await blah.json();
-        this.setState({ numVerify });
-        // fetch map data
+        try {
+          const numVerifyRequest = await fetch(
+            `${numVeryifyUrl}${phoneNumber}`
+          );
+          const numVerify = await numVerifyRequest.json();
+          this.setState({ numVerify });
+          // fetch map data
+          const mapBoxGeoRequest = await fetch(
+            this.mapBoxGeoUrl(this.state.numVerify)
+          );
+          const mapBoxLocation = await mapBoxGeoRequest.json();
+          this.setState({ location: mapBoxLocation.features[0] });
+        } catch (e) {
+          console.log(e);
+        }
       }
     }
   }
 
   render() {
+    if (!this.state.numVerify.number) return null;
     return (
       <div>
         <NumberInformationComponent
@@ -37,8 +62,7 @@ export default class CustomCrmContent extends Component {
         <LocationInformationComponent
           key="location-info"
           {...this.props}
-          center={this.props.center}
-          zoom={this.props.zoom}
+          center={this.state.location.center}
         />
       </div>
     );
--- src/Hacknight1Plugin.js
+++ src/Hacknight1Plugin.js
@@ -4,71 +4,14 @@ import CustomTaskListComponent from "./CustomTaskListComponent";
 import CustomTaskInfoPanelItem from "./CustomTaskInfoPanelItem";
 import CustomMapCMSReplace, { accessToken } from "./CustomMapCMSReplace";
 import CustomCrmContent from "./CustomCrmContent";
-// import NumberInformationComponent from "./NumberInformationComponent";
-// import LocationInformationComponent from "./LocationInformationComponent";
 
 const PLUGIN_NAME = "Hacknight1Plugin";
-
-const mapBoxGeoUrl = () =>
-  `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(
-    this.state.data.location
-  )}.json?access_token=${accessToken}&country=${
-    this.state.data.country_code
-  }&autocomplete=true&limit=1`;
 
 /* <link href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" rel="stylesheet" />; */
 
 export default class Hacknight1Plugin extends FlexPlugin {
   constructor() {
     super(PLUGIN_NAME);
-    this.state = {
-      data: { location: "Salt Lake City", country_code: "US" },
-      center: [-111.8904, 40.767],
-      match: {
-        type: "FeatureCollection",
-        query: ["salt", "lake", "city"],
-        features: [
-          {
-            id: "place.11659519155508240",
-            type: "Feature",
-            place_type: ["place"],
-            relevance: 1,
-            properties: {
-              wikidata: "Q23337"
-            },
-            text: "Salt Lake City",
-            place_name: "Salt Lake City, Utah, United States",
-            bbox: [
-              -112.176070665723,
-              40.5770690001977,
-              -111.553103050872,
-              40.9220496017179
-            ],
-            center: [-111.8904, 40.767],
-            geometry: {
-              type: "Point",
-              coordinates: [-111.8904, 40.767]
-            },
-            context: [
-              {
-                id: "region.216168",
-                short_code: "US-UT",
-                wikidata: "Q829",
-                text: "Utah"
-              },
-              {
-                id: "country.3145",
-                short_code: "us",
-                wikidata: "Q30",
-                text: "United States"
-              }
-            ]
-          }
-        ],
-        attribution:
-          "NOTICE: © 2018 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained. POI(s) provided by Foursquare."
-      }
-    };
   }
 
   /**
@@ -88,24 +31,6 @@ export default class Hacknight1Plugin extends FlexPlugin {
     flex.TaskInfoPanel.Content.add(
       <CustomTaskInfoPanelItem key="steps-for-task" />
     );
-    // flex.CRMContainer.defaultProps.uriCallback = task =>
-    //   fetch(`${numVeryifyUrl}${task.attributes.name}`)
-    //     .then(response => response.JSON())
-    //     .then(data => this.setState({ data }))
-    //     .then(() =>
-    //       fetch(mapBoxGeoUrl)
-    //         .forwardGeocode({
-    //           query: this.state.data.location,
-    //           countries: [this.state.data.country_code],
-    //           limit: 1
-    //         })
-    //         .send()
-    //         .then(response => response.body)
-    //         .catch(e => console.log(e))
-    //     )
-    //     .catch(e => console.log(e));
-    flex.CRMContainer.Content.replace(
-      <CustomCrmContent {...this.state} key="custom-crm" />
-    );
+    flex.CRMContainer.Content.replace(<CustomCrmContent key="custom-crm" />);
   }
 }
